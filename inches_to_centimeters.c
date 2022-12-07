2e77101949d9e172a035ac88c7228c544ee2dd16
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2022-12-07 18:48:54 +0530

    #include<stdio.h> int main() {     int h;     scanf("%d",&h);     float s;     s=h*2.54;     printf("%0.2f",s); }

--- inches_to_centimeters.c
+++ inches_to_centimeters.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int n;
-    scanf("%d",&n);
-    float t;
-    t=2.54*n;
-    printf("%0.2f",t);
+    int h;
+    scanf("%d",&h);
+    float s;
+    s=h*2.54;
+    printf("%0.2f",s);
 }
