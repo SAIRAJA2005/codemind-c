9a54e898a8b52a3d4aa22c58a35343e84bb73243
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2023-01-01 22:11:47 +0530

    #include<stdio.h> int main() {    int n,i;    scanf("%d",&n);    for(i=1;i<=n;i++)    {        printf("%d ",i);    } }

--- N_natural_numbers.c
+++ N_natural_numbers.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    int n,i;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    {
-        printf("%d ",i);
-    }
+   int n,i;
+   scanf("%d",&n);
+   for(i=1;i<=n;i++)
+   {
+       printf("%d ",i);
+   }
 }
