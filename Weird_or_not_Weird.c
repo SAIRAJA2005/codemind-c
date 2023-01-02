368a5815a4f77282db3635048ae8909bf745240a
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2023-01-02 22:10:02 +0530

    #include<stdio.h> int weird(int n) {  if(n%2!=0)   {    return 1;   }    else if (n%2==0&&n>2&&n<5)    {     return 2;    }     else if(n%2==0&&n>6&&n<20)    {    return 3;    }    else    {     return 4; 	} } int main() {  int n;  scanf("%d",&n);   int x=weird(n);    if(x==1||x==3)      { 	   printf("weird"); 	 } 	 else   {   printf("not weird");   } }

--- Weird_or_not_Weird.c
+++ Weird_or_not_Weird.c
@@ -1,14 +1,34 @@
 #include<stdio.h>
+int weird(int n)
+{
+ if(n%2!=0)
+  {
+   return 1;
+  }
+   else if (n%2==0&&n>2&&n<5) 
+   {
+    return 2; 
+   }
+    else if(n%2==0&&n>6&&n<20) 
+   { 
+   return 3;
+   } 
+   else 
+   {
+    return 4; 
+	}
+}
 int main()
 {
-    int n;
-    scanf("%d",&n);
-    if(n%2!=0)
-    printf("weird");
-    else if (n%2==0&&n>2&&n<5)
-    printf("not weird");
-    else if (n%2==0&&n>6&&n<20)
-    printf("weird");
-    else 
-    printf("not weird");
+ int n; 
+ scanf("%d",&n);
+  int x=weird(n);
+   if(x==1||x==3)
+     {
+	   printf("weird");
+	 } 
+	 else
+  { 
+  printf("not weird"); 
+  }
 }
