5011ef5545aacad573a9c5bad25b39020826cd45
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2023-01-05 14:40:42 +0530

    #include<stdio.h> int main() {     int n,i,d=0,s=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {        scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(i%2==0)         s=s+a[i];         else         d=d+a[i];     }     if(s>d)     printf("%d",s-d);     else     printf("%d",d-s); }

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_indexed_elements.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,d=0,s=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+       scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i%2==0)
+        s=s+a[i];
+        else
+        d=d+a[i];
+    }
+    if(s>d)
+    printf("%d",s-d);
+    else
+    printf("%d",d-s);
+}
