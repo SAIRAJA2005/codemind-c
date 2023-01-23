ccb554026a606a7b40272a531921f4fca80314a7
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2023-01-23 20:12:11 +0530

    #include<stdio.h> int main() {     int a;     float b,c;     scanf("%d",&a);     b=3.14*a*a;     c=2*3.14*a;     printf("%.2f %0.2f",b,c); }

--- Area_and_Perimeter_of_Circle.c
+++ Area_and_Perimeter_of_Circle.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int n;
-    scanf("%d",&n);
-    float j,i;
-    j=3.14*n*n;
-    i=2*3.14*n;
-    printf("%0.2f
-%0.2f",j,i);
+    int a;
+    float b,c;
+    scanf("%d",&a);
+    b=3.14*a*a;
+    c=2*3.14*a;
+    printf("%.2f
+%0.2f",b,c);
 }
