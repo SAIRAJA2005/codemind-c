133623d3bc7bb829d7c0540c20e84bc397d37712
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2023-06-16 01:09:34 +0530

    #include<stdio.h> int fun(int n) {     int i;     for(i=2;i<n/2;i++)     {         if (n%i==0) return 0;     }     return 1; } int main() {     int n,k,k1,i,c=0,p;     scanf("%d",&n);     for (i=2;i<n/2;i++)     {         if (n%i==0)         {             p = i;             if (fun(p)==1)             {                 k=n/p;                 if (fun(k)==1)                 {                     c=1;                     k1 = k;                     printf("%d %d",p,k1);                     break;                 }             }         }     }     if (c==0) printf("-1"); }

--- Find_two_distinct_prime_numbers_with_given_product.c
+++ Find_two_distinct_prime_numbers_with_given_product.c
@@ -2,23 +2,29 @@
 int fun(int n)
 {
     int i;
-    for (i=2;i<=n/2;i++){
+    for(i=2;i<n/2;i++)
+    {
         if (n%i==0) return 0;
     }
     return 1;
 }
 int main()
 {
-    int n,i,c=0,r,k;
+    int n,k,k1,i,c=0,p;
     scanf("%d",&n);
-    for (i=2;i<n;i++){
-        if (n%i==0){ 
-                 r = i;
-            if (fun(r)==1){
-                    k = n/r;
-                if (fun(k)==1){
-                    c = 1;
-                    printf("%d %d",r,k);
+    for (i=2;i<n/2;i++)
+    {
+        if (n%i==0)
+        {
+            p = i;
+            if (fun(p)==1)
+            {
+                k=n/p;
+                if (fun(k)==1)
+                {
+                    c=1;
+                    k1 = k;
+                    printf("%d %d",p,k1);
                     break;
                 }
             }
