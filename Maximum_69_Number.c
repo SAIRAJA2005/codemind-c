9ac531fa8565f63692b8305a1641ea5139255bb6
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2023-06-17 04:21:41 +0530

    #include<stdio.h> #include<string.h> int main() {     char n[100],i;     scanf("%s",n);     for(i=0;i<strlen(n);i++){         if (n[i]=='6'){             n[i]='9';             break;         }     } printf("%s",n); }

--- Maximum_69_Number.c
+++ Maximum_69_Number.c
@@ -3,12 +3,11 @@
 int main()
 {
     char n[100],i;
-    scanf("%s",&n);
-    for (i=0;i<strlen(n);i++){
+    scanf("%s",n);
+    for(i=0;i<strlen(n);i++){
         if (n[i]=='6'){
             n[i]='9';
             break;
         }
-    }
-    printf("%s",n);
+    } printf("%s",n);
 }
