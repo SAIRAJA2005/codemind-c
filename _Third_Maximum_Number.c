03ec8d1607735efc54d28a09141f8242f9927ba2
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2023-06-16 17:58:05 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++){         scanf("%d",&arr[i]);     }     int m=0;     for(i=0;i<n;i++){         if(arr[i]>m){             m = arr[i];         }     }     int m1 = 0;     for(i=0;i<n;i++){         if(arr[i]<m && arr[i]>m1){             m1 = arr[i];         }     }     int m2=0;     for(i=0;i<n;i++){         if(arr[i]<m && arr[i]<m1 && arr[i]>m2){             m2 = arr[i];         }     }     if(m2==0){         printf("%d",m);     }     else{         printf("%d",m2);     } }

---
+++ _Third_Maximum_Number.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int m=0;
+    for(i=0;i<n;i++){
+        if(arr[i]>m){
+            m = arr[i];
+        }
+    }
+    int m1 = 0;
+    for(i=0;i<n;i++){
+        if(arr[i]<m && arr[i]>m1){
+            m1 = arr[i];
+        }
+    }
+    int m2=0;
+    for(i=0;i<n;i++){
+        if(arr[i]<m && arr[i]<m1 && arr[i]>m2){
+            m2 = arr[i];
+        }
+    }
+    if(m2==0){
+        printf("%d",m);
+    }
+    else{
+        printf("%d",m2);
+    }
+}
