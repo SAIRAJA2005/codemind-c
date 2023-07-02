77d23c2e5f0a66c086724ba396355f26d8c6157f
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2023-07-02 09:07:37 +0530

    #include<stdio.h> int main() {     int m , n , i;     scanf("%d%d",&m,&n);     for (i=1;i<=n;i++){         if ((m*i)%n==0){             printf("%d",i*m);             break;         }     } }

---
+++ Least_Common_Multiple.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int m , n , i;
+    scanf("%d%d",&m,&n);
+    for (i=1;i<=n;i++){
+        if ((m*i)%n==0){
+            printf("%d",i*m);
+            break;
+        }
+    }
+}
