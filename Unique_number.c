bb5949d0abd05e524f0152dbc84350e47b1128fc
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2023-07-10 14:35:25 +0530

    #include<stdio.h> #include<string.h> int main(){     int c,k=0,i,j;char n[100];scanf("%s",n);     for(i=0;i<strlen(n);i++){         c=0; for(j=0;j<strlen(n);j++){             if(n[i]==n[j]) c++;         }         if(c!=1){             k=1; printf("Not Unique Number"); break;         }     }     if(k==0) printf("Unique Number"); }

--- Unique_number.c
+++ Unique_number.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-    char s[100]; scanf("%s",s); int i , c , j ,f=0;
-    for (i=0;i<strlen(s);i++){
-        c = 0;
-        for (j=0;j<strlen(s);j++){
-            if (s[i]==s[j]) c++;
+    int c,k=0,i,j;char n[100];scanf("%s",n);
+    for(i=0;i<strlen(n);i++){
+        c=0; for(j=0;j<strlen(n);j++){
+            if(n[i]==n[j]) c++;
         }
-        if (c!=1) {
-            f = 1; printf("Not Unique Number");  break;
+        if(c!=1){
+            k=1; printf("Not Unique Number"); break;
         }
     }
-    if (f==0) printf("Unique Number");
+    if(k==0) printf("Unique Number");
 }
