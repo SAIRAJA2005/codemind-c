fb4fcf889de8117109aa90a13c48f51f217cff51
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2023-07-10 15:11:17 +0530

    #include<stdio.h> int main(){     int n;scanf("%d",&n);int k = n,r, count = 0;     while(n>0){         r = n%10;count++;n = n/10;     }     n = k;     int l , e = 0 , o = 0 , m = 0;     while(n!=0){         l = n%10;         if (l%2==0) e++;         else if (l%2!=0) o++;         else m++;         n = n/10;     }     if (e == count) printf("Even");     else if (o == count) printf("Odd");     else printf("Mixed"); }

--- Even_Odd_Mixed.c
+++ Even_Odd_Mixed.c
@@ -1,37 +1,19 @@
 #include<stdio.h>
-int main()
-{
-    int n;
-    scanf("%d",&n);
-    int k = n;
-    int r, count = 0;
+int main(){
+    int n;scanf("%d",&n);int k = n,r, count = 0;
     while(n>0){
-        r = n%10;
-        count++;
-        n = n/10;
+        r = n%10;count++;n = n/10;
     }
     n = k;
     int l , e = 0 , o = 0 , m = 0;
     while(n!=0){
         l = n%10;
-        if (l%2==0){
-            e++;
-        }
-        else if (l%2!=0){
-            o++;
-        }
-        else{
-            m++;
-        }
+        if (l%2==0) e++;
+        else if (l%2!=0) o++;
+        else m++;
         n = n/10;
     }
-    if (e == count){
-        printf("Even");
-    }
-    else if (o == count){
-        printf("Odd");
-    }
-    else{
-        printf("Mixed");
-    }
+    if (e == count) printf("Even");
+    else if (o == count) printf("Odd");
+    else printf("Mixed");
 }
