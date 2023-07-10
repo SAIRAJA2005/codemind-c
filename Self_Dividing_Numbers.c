bc078204f6e6696ec8cfe5ff7b1f1fa049c788e0
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2023-07-10 13:30:49 +0530

    #include<stdio.h> int fun(int n){     int q=n,r;while (q!=0){         r=q%10;if (r==0){             return 0;         }         else if(n%r!=0){             return 0;         }         q=q/10;     }     return 1; } int main() {     int n,m,i;scanf("%d%d",&n,&m);for(i=n;i<=m;i++){         if (fun(i)==1) printf("%d ",i);     } }

--- Self_Dividing_Numbers.c
+++ Self_Dividing_Numbers.c
@@ -1,32 +1,19 @@
 #include<stdio.h>
-int fun(int n)
-{
-    int p=n,r;
-    while(n!=0)
-    {
-        r = n%10;
-        if (r==0)
-        {
+int fun(int n){
+    int q=n,r;while (q!=0){
+        r=q%10;if (r==0){
             return 0;
         }
-        else if(p%r!=0)
-        {
+        else if(n%r!=0){
             return 0;
         }
-        n = n/10;
+        q=q/10;
     }
     return 1;
 }
 int main()
 {
-    int n,m,i;
-    scanf("%d",&n);
-    scanf("%d",&m);
-    for (i=n;i<=m;i++)
-    {
-        if (fun(i)==1)
-        {
-            printf("%d ",i);
-        }
-    }
+    int n,m,i;scanf("%d%d",&n,&m);for(i=n;i<=m;i++){
+        if (fun(i)==1) printf("%d ",i);
+    }    
 }
